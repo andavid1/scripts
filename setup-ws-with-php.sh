79d3fbc3b45b3e144b6b621de39f858746c73bfd
davidanands <david.anands@gmail.com>
2014-11-04 20:14:19 +0100

    fixed the install

--- setup-ws-with-php.sh
+++ setup-ws-with-php.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
-#installer for apache and php in ubuntu only 
-#version 1.0 
+#!/bin/bash
+#installer for apache and php in ubuntu only
+#version 1.0
 #Author David Anand <david.anands@gmail.com>
-set -x 
+#set -x
 hash -r
-ulimit -H -c0
-set -euf -o pipefail
+#ulimit -H -c0
+#set -euf -o pipefail
 #Declerations
 net=`ping -c 3 yahoo.com | grep -i "loss" | awk '{ print $6 }'|  sed 's/%/ /'`
 apa='apache2 php5'
@@ -30,40 +30,41 @@ exit
 fi
 
 if [ $WISH = "y" ]; then
-echo starting installation 
+echo starting installation
 fi
 
+app ()
+{
+   if [ -d "/home/$USER/test-app" ]
+   then
+      echo "fixing permissions..."
+      chown -R $USER.$USER /home/$USER/test-app
+      echo "cerating symlinks..."
+      sudo ln -s /home/$USER/test-app /var/www/html
+   else
+      echo "Error: Directory does not exists please place the files and rerun the script"
+      exit
+   fi
+}
 
-function install
-{	
+
+appinst ()
+{
   sudo apt-get update >/dev/null
   sudo dpkg -l $apa > /dev/null 2>&1
   INSTALLED=$?
-  if [ $INSTALLED = 0 ]; then
-  echo apache $apa already installed skipping $apa installation
-  application
-  else
-  echo updating the system...
-  sudo apt-get install apache2 php5 -y >/dev/null
-  sleep 100
-  sudo a2enmod ssl
-  sudo a2ensite default-ssl
-  sudo service apache2 restart
-  application
-  fi
+
+    if [ $INSTALLED = 0 ];then
+       echo " $apa already installed skipping installation"
+       else
+       echo "updating the system..."
+       sudo apt-get install $apa -y >/dev/null
+       sudo a2enmod ssl
+       sudo a2ensite default-ssl
+       sudo service apache2 restart
+      fi
+app;
 }
 
-function application 
-{
-	if [ -d "/home/$USER/test-app" ];then
-    echo fixing permissions...
-    chown -R $USER.USER /home/$USER/test-app
-    sudo ln -s /home/$USER/test-app /var/www
-    else
-    echo "Error: Directory does not exists please place the files and rerun the script"
-    exit 
-    fi
-    }
-
-install 
+appinst;
 
